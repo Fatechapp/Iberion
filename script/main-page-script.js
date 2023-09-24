14368f1a65ad9f3fe5c1cdde6e2e2be12bbeb5ce
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2023-09-24 21:47:32 +0700

    Update main-page-script.js

--- script/main-page-script.js
+++ script/main-page-script.js
@@ -2,9 +2,13 @@
 //toggle
 const body = document.querySelector('body')
 const toggle = document.getElementById('toggle')
+const penyelenggara = document.getElementById('Penyelenggara');
+const gambaran = document.getElementById('Gambaran')
 toggle.onclick = function() {
     toggle.classList.toggle('active')
     body.classList.toggle('active')
+    penyelenggara.classList.toggle('active')
+    gambaran.classList.toggle('active')
     // alert("Fitur belum tersedia")
     errorAllert.style.display = 'block';
     setTimeout(function() {
